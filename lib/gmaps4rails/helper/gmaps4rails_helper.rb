04b5adba48d19215c272c5b1c32173f7d8fb6239
Werner Schmitt <wfschmittx@gmail.com>
2013-11-07 01:23:59 +0100

    with auto adjust

--- lib/gmaps4rails/helper/gmaps4rails_helper.rb
+++ lib/gmaps4rails/helper/gmaps4rails_helper.rb
@@ -4,7 +4,7 @@ module Gmaps4railsHelper
   # @params [String] builder is a json string
   def gmaps4rails(builder)
     options = {
-      :map_options => { :auto_adjust => true, :auto_zoom => false, :zoom => 10 },
+      :map_options => { :auto_adjust => true, :auto_zoom => false, :zoom => 13 },
       :markers     => { :data => builder, :options => {:do_clustering => true} }
     }
     gmaps(options)
